5ee5bb4afac256a47e2e97962cd0a94ad699c52b
shrish0 <shrishgupta1234@gmail.com>
2024-08-02 12:23:54 +0530

    Requisition Model

---
+++ Controllers/ManageRequisitionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorkFlow.DataAccess.Data;
+using WorkFlow.Models;
+using WorkFlow.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Security.Claims;
+
+namespace WorkFlowWeb.Controllers
+{
+
+}
